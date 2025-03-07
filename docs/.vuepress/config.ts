5bd27aed434bb89aac18ae4b7b4dcd9661437890
Junbang-Stack <junbang555@gmail.com>
2025-03-07 09:45:23 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/jblog/',
   lang: 'zh-CN',
   title: 'junbang&#x27;s private',
   description: 'junbang&#x27;s private',
