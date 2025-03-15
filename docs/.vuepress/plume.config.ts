bb4c0720b683e21c76b2c739ce09a697de5de0d1
junbang <junbang555@gmail.com>
2025-03-15 11:50:42 +0800

    优化更新

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -12,7 +12,6 @@ export default defineThemeConfig({
 
   social: [
     { icon: 'github', link: 'https://github.com/Junbang-Stack' },
-    { icon: 'email', link: 'mailto:junbang555@163.com' },
   ],
    navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
    aside: true, // 页内侧边栏， 默认显示在右侧
